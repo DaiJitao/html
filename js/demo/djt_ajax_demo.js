5766dac18c4f3d7ac3803b79a94a46161ce7a8d3
daijitao <daijitao@ctsi.com.cn>
2018-05-22 15:19:28 +0800

    Initial commit

---
+++ js/demo/djt_ajax_demo.js
@@ -0,0 +1,119 @@
+function serialize(form) {
+    var parts = [],
+        field = null,
+        i,
+        len,
+        j,
+        optLen,
+        option,
+        optValue;
+    for (i = 0, len = form.elements.length; i < len; i++) {
+        field = form.elements[i];
+        switch (field.type) {
+            case "select-one":
+            case "select-multiple":
+                if (field.name.length) {
+                    for (j = 0, optLen = field.options.length; j < optLen; j++) {
+                        option = field.options[j];
+                        if (option.selected) {
+                            optValue = "";
+                            if (option.hasAttribute) {
+                                optValue = (option.hasAttribute("value") ?
+                                    option.value : option.text);
+                            } else {
+                                optValue = (option.attributes["value"].specified ?
+                                    option.value : option.text);
+                            }
+                            parts.push(encodeURIComponent(field.name) + "=" +
+                                encodeURIComponent(optValue));
+                        }
+                    }
+                }
+                break;
+            case undefined:
+            case "file":
+            case "submit":
+            case "reset":
+            case "button":
+                break;
+            case "radio":
+            case "checkbox":
+                if (!field.checked) {
+                    break;
+                }
+            default:
+                if (field.name.length) {
+                    parts.push(encodeURIComponent(field.name) + "=" +
+                        encodeURIComponent(field.value));
+                }
+        }
+    }
+    return parts.join("&");
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 产生 XMLHttpRequest
+function createXHR() {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
+                    "MSXML2.XMLHttp"],
+                i, len;
+            for (i = 0, len = versions.length; i < len; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+                    // 跳过
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error("No XHR object available.");
+    }
+}
+
+var xhr = createXHR();
+xhr.onreadystatechange = function () {
+    if (shr.readyState == 4) {
+        if ((xhr.status >= 200 && xhr <= 300) || xhr.status == 304) {
+            alert(xhr.responseText)
+        } else {
+            alert("Request was unsuccessful: " + xhr.status)
+        }
+    }
+};
+
+// GFT 请求，参数的编码
+function addURLParam(url, param, value) {
+    url += (url.indexOf("?") == -1 ? "?" : "&"); // 如果没有问号，则添加问号；如有，则添加&
+    url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    return url;
+};
+// 用法 xhr.open("get", "example.php", false);
+xhr.open("get", "url", true)
+xhr.send("daijitao") // xhr.send(null)
+
+// POST请求 // 用于表单提交数据
+function submitData() {
+    var xhr = createXHR();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+                alert(xhr.responseText);
+            } else {
+                alert("Request was unsuccessful: " + xhr.status);
+            }
+        }
+    };
+    // 用于表单提交数据
+    xhr.open("post", "postexample.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var form = document.getElementById("user-info");
+    xhr.send(serialize(form));
+    // 如果上述序列化不可用，建议采取下面的方法
+    xhr.send(new FormData(form))
+}
